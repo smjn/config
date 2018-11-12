0cb6cfe30932757ed89453f7cce41ab06d686702
Sushant Mahajan <sushant.mahajan88@gmail.com>
2018-11-13 00:04:58 +0530

    fixed installer name

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,6 @@ fi
 if sudo apt -y update && sudo apt -y install git; then
     git clone https://github.com/smjn/config $HOME/.dotfiles
     cd $HOME/.dotfiles
-    ./install $HOME
+    ./install.sh $HOME
 fi
 
