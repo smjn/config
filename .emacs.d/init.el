3bf323c28fb73246aba5b52d9c6cdc8e1938caf3
sushant mahajan <sushant.mahajan88@gmail.com>
2021-06-28 19:06:17 +0000

    custom emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,53 +1,26 @@
-(setq inhibit-startup-message t)  ; disable initial screen
-
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(tooltip-mode -1)
-(set-fringe-mode 10)              ; little bit of padding around the work area
+;; general UI
 (menu-bar-mode -1)
-
-(setq visible-bell t)
-
-(setq vc-follow-symlinks t)
-
-(setq custom-file (concat user-emacs-directory "/custom.el"))
-
-(when (file-exists-p custom-file)
-   (load-file custom-file))
-
-(set-face-attribute 'default  nil :font "Fira Code" :height 105)
-
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
+(setq inhibit-startup-message t)
+(setq inhibit-splash-screen t)
 (setq display-line-numbers-type 'relative)
-(global-display-line-numbers-mode t)
+(global-display-line-numbers-mode 1)
 
-(dolist (mode '(org-mode-hook
-		eshell-mode-hook
-		shell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
-(global-set-key (kbd "C-c l") 'display-line-numbers-mode)
+;; custom files and paths
+(setq locale-coding-system 'utf-8)
+(setq custom-file (concat user-emacs-directory "/custom.el"))
+(when (file-exists-p custom-file)
+  (load-file custom-file))
 
-;; UTF-8 support
-(prefer-coding-system       'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-
-(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
-
-;; init package sources
+;; custom package archives
 (require 'package)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/" ) t)
-(add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/") t)
-
+;; package installation boilerplate
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
-
-;; init package sources
 
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
@@ -55,9 +28,11 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-
+;; custom packages
 (use-package swiper)
 
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
@@ -71,30 +46,19 @@
   :after (swiper)
   :bind (("C-s" . swiper)
 	 :map ivy-minibuffer-map
-	 ("TAB" . ivy-alt-done))
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
   :config
   (ivy-mode 1))
-
-
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 14)))
-
-
-(use-package doom-themes)
-(load-theme 'doom-gruvbox 1)
-
-
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-dle-timeout 0.3))
 
 
 (use-package ivy-rich
@@ -103,53 +67,52 @@
   (ivy-rich-mode 1))
 
 
-(use-package helpful
-  :ensure t
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
-  
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-timeout 0.3))
+
 
 (use-package evil
-  :ensure t
   :init
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
   (setq evil-want-C-u-scroll t)
   :config
-  (evil-mode 1))
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state))
 
 
 (use-package company
   :ensure t
-  :config
-  (add-hook 'after-init-hook 'global-company-mode))
-
-
-(use-package projectile
-  :diminish
-  :config (projectile-mode)
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
   :init
-  (when (file-directory-p "~/dev")
-    (setq projectile-project-search-path '("~/dev")))
-  (setq projectile-switch-project-action #'projectile-dired))
+  (add-hook 'after-init-hook 'global-company-mode)
+  (setq company-idle-delay nil))
 
 
-(use-package counsel-projectile
-  :config (counsel-projectile-mode))
+(use-package doom-themes
+  :ensure t
+  :init
+  (load-theme 'doom-gruvbox t))
 
 
-(use-package magit
-  :custom
-  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-vi))
+(use-package general
+  :ensure t
+  :config
+  (general-evil-setup t)
+  (general-create-definer smjn/leader-keys
+    :keymaps '(normal)
+    :prefix "SPC")
 
-
-(use-package evil-magit
-  :after magit)
+  (smjn/leader-keys
+   "t" '(:ignore t :which-key "toggles")
+   "tt" '(counsel-load-theme :which-key "choose theme")
+   "tl" 'display-line-numbers-mode
+   "tc" '((lambda () (interactive)
+	   (if company-idle-delay
+	       (setq company-idle-delay nil)
+	     (setq company-idle-delay 0.1))) :which-key "toggle company mode")
+   "ts" '(evil-ex-nohighlight :which-key "toggle hlsearch")
+   "b" '(:ignore b :which-key "buffers")
+   "bl" '(counsel-switch-buffer :which-key "choose buffer")))
