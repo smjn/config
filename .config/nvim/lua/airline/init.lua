eeb382fb34a4eca943e4059d7bbf7a5a161ff6bf
Sushant Mahajan <sushant.mahajan88@gmail.com>
2022-09-29 16:23:32 +0530

    Use lua based config for ale globals.

--- .config/nvim/lua/airline/init.lua
+++
@@ -1,5 +0,0 @@
-vim.cmd [[
-    let g:airline#extensions#tabline#enabled = 1
-    let g:airline#extensions#branch#enabled = 1
-    let g:airline_powerline_fonts = 1
-]]
--- .config/nvim/lua/ale/init.lua
+++ .config/nvim/lua/ale/init.lua
@@ -1,5 +1,20 @@
-vim.cmd [[
-    let g:ale_linters = {'python': ['flake8', 'pydocstyle'], 'sh': ['shellcheck'], 'zsh': ['shellcheck'], 'lua': ['luacheck'], 'c': ['cpplint'], 'cpp': ['cpplint']} 
-    let g:ale_fixers = {'*': [], 'python': ['black', 'isort'], 'sh': ['shfmt'], 'zsh': ['shfmt'], 'lua': ['lua-format'], 'c': ['clang-format'], 'cpp': ['clang-format']}
-    let g:ale_fix_on_save = 1
-]]
+vim.g.ale_linters = {
+    python = {'flake8', 'pydocstyle'},
+    sh = {'shellcheck'},
+    zsh = {'shellcheck'},
+    lua = {'luacheck'},
+    c = {'cpplint'},
+    cpp = {'cpplint'}
+}
+
+vim.g.ale_fixers = {
+    ['*'] = {},
+    python = {'black', 'isort'},
+    sh = {'shfmt'},
+    zsh = {'shfmt'},
+    lua = {'lua-format'},
+    c = {'clang-format'},
+    cpp = {'clang-format'}
+}
+
+vim.g.ale_fix_on_save = 1
